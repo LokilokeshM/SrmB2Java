7fe0e02053fbfd62d763aa8ee2fa06e3d6a99e99
LokilokeshM <78999166+LokilokeshM@users.noreply.github.com>
2021-06-24 09:34:49 +0530

    Updated

---
+++ Day22/Number.java
@@ -0,0 +1,43 @@
+package Day22;
+
+public class Number {
+
+	public boolean Zero(){
+		
+		return false;
+		}
+	public boolean Positive(){
+		return false;
+		}
+	public boolean Negative(){
+		return false;
+		}
+	public boolean Odd( ){
+		return false;
+		}
+	public boolean Even( ){
+		return false;
+		}
+	public boolean Prime(){
+		return false;
+		}
+	public boolean Amstrong(){
+		return false;
+		} 
+
+	public double Factorial(){
+		return 10*(10+1);
+		}
+	public double GetSqrt(){
+		return 5^5;
+		}
+	public double GetSqr(){
+		return 0;
+		}
+	public double SumDigits(){
+		return 10+19;
+		} 
+	public double GetReverse(){
+		return 435321354;
+		}
+}
---
+++ Day22/NumberMainClass.java
@@ -0,0 +1,20 @@
+package Day22;
+
+public class NumberMainClass {
+	public static void main(String[] args)
+	{
+		Number n = new Number();
+		
+		System.out.println("Factorial"+n.Factorial());
+		System.out.println("Square Root"+n.GetSqr());
+		System.out.println("Square"+n.GetSqr());
+		System.out.println("Sum Digits"+n.SumDigits());
+		System.out.println("Get Reverse"+n.Zero());
+		System.out.println("Zero"+n.Zero());
+		System.out.println("Positive"+n.Positive());
+		System.out.println("Negative"+n.Negative());
+		System.out.println("Odd"+n.Odd());
+		System.out.println("Even"+n.Even());
+		System.out.println("Prime"+n.Prime());
+	}
+}
